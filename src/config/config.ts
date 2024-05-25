183db05f18a48f08358803aea901b6fce4e6135d
jbotero <jbotero0808@gmail.com>
2024-05-25 10:18:13 -0500

    Mensaje1

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  port: process.env.PORT || 4000,
+  port: process.env.PORT || 3000,
   dbUri: process.env.MONGODB_URI ,
   jwtSecret: process.env.JWT_SECRET || '',
   environment: process.env.NODE_ENV || '',
